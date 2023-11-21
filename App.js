d4d928145cc99dc4db73328d407be3e608a6572a
Mauricio Mercado <mau@maumercado.com>
2023-11-21 11:42:36 -0800

    Add custom button to header

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { StatusBar } from 'react-native'
+import { StatusBar, Button } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 
--- screens/MealDetailsScreen.js
+++ screens/MealDetailsScreen.js
@@ -1,13 +1,35 @@
-import { View, Image, Text, StyleSheet, ScrollView } from 'react-native'
-import MealDetails from '../components/MealDetails'
+import { useLayoutEffect } from 'react'
+import { View, Image, Text, StyleSheet, ScrollView, Button } from 'react-native'
 
 import { MEALS } from '../data/dummy-data'
+
+import MealDetails from '../components/MealDetails'
 import Subtitle from '../components/MealDetail/Subtitle'
 import List from '../components/MealDetail/List'
+import IconButton from '../components/IconButton'
 
-function MealDetailsScreen ({ route }) {
+function MealDetailsScreen ({ route, navigation }) {
   const { mealId } = route.params
   const selectedMeal = MEALS.find(meal => meal.id === mealId)
+
+  function headerButtonPressHandler () {
+    console.log("pressed")
+  }
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: () => {
+        return (
+          <IconButton
+            icon="star"
+            color="white"
+            onPress={headerButtonPressHandler}
+          />
+        )
+      }
+    })
+  }, [navigation, headerButtonPressHandler])
+
   return (
     <ScrollView style={styles.rootContainer}>
       <Image style={styles.image} source={{ uri: selectedMeal.imageUrl }} />
