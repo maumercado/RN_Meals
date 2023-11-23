9c03df22438980762da53f147b3eebb62b65e202
Mauricio Mercado <mau@maumercado.com>
2023-11-23 12:15:34 -0800

    Add Drawer Navigator

--- App.js
+++ App.js
@@ -1,12 +1,36 @@
-import { StatusBar, Button } from 'react-native'
+import 'react-native-gesture-handler';
+
+import { StatusBar } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { createDrawerNavigator } from '@react-navigation/drawer'
 
 import CategoriesScreen from './screens/CategoriesScreen'
 import MealsOverviewScreen from './screens/MealsOverviewScreen'
 import MealDetailsScreen from './screens/MealDetailsScreen'
+import FavouritesScreen from './screens/FavouritesScreen';
 
 const Stack = createNativeStackNavigator()
+const Drawer = createDrawerNavigator()
+
+function DrawerNavigator () {
+  return (
+    <Drawer.Navigator
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: '#24180f'
+        },
+        headerTintColor: 'white',
+        sceneContainerStyle: {
+          backgroundColor: '#2b1e13'
+        }
+      }}
+    >
+      <Drawer.Screen name='Categories' component={CategoriesScreen} />
+      <Drawer.Screen name='Favourites' component={FavouritesScreen} />
+    </Drawer.Navigator>
+  )
+}
 
 export default function App () {
   return (
@@ -25,10 +49,10 @@ export default function App () {
           }}
         >
           <Stack.Screen
-            name='MealsCategories'
-            component={CategoriesScreen}
+            name='Drawer'
+            component={DrawerNavigator}
             options={{
-              title: 'Meal Categories'
+              headerShown: false
             }}
           />
           <Stack.Screen
---
+++ screens/FavouritesScreen.js
@@ -0,0 +1,10 @@
+import {Text} from 'react-native'
+
+
+function FavouritesScreen () {
+  return (
+    <Text>FavouritesScreen</Text>
+  )
+}
+
+export default FavouritesScreen
