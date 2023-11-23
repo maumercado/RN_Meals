a1583825c32daf16eef0c9c7da81f70d46736ad5
Mauricio Mercado <mau@maumercado.com>
2023-11-23 15:25:30 -0800

    lint

--- screens/FavouritesScreen.js
+++ screens/FavouritesScreen.js
@@ -1,4 +1,5 @@
 import { useContext } from 'react'
+import { View, Text, StyleSheet } from 'react-native'
 
 import { MEALS } from '../data/dummy-data'
 
@@ -9,11 +10,32 @@ function FavouritesScreen () {
   const favouritesMealsCtx = useContext(FavouritesContext)
   const favouriteMeals = MEALS.filter(meal => favouritesMealsCtx.ids.some(id => id === meal.id))
 
+  if (!favouriteMeals.length) {
+    return (
+      <View style={styles.rootContainer}>
+        <Text style={styles.text}>No favourite meals found!</Text>
+      </View>
+    )
+  }
+
   return (
     <MealsList
       items={favouriteMeals}
     />
   )
 }
+
+const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  text: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 18
+  }
+})
 
 export default FavouritesScreen
