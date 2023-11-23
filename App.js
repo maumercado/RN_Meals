f211535d9c7c9e1005b4a6384e5fe54de21e89e9
Mauricio Mercado <mau@maumercado.com>
2023-11-23 12:22:36 -0800

    Add styling

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { StatusBar } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { createDrawerNavigator } from '@react-navigation/drawer'
+import { Ionicons } from '@expo/vector-icons'
 
 import CategoriesScreen from './screens/CategoriesScreen'
 import MealsOverviewScreen from './screens/MealsOverviewScreen'
@@ -23,17 +24,42 @@ function DrawerNavigator () {
         headerTintColor: 'white',
         sceneContainerStyle: {
           backgroundColor: '#2b1e13'
-        }
+        },
+        drawerContentStyle: {
+          backgroundColor: '#2b1e13'
+        },
+        drawerActiveTintColor: '#3f2f25',
+        drawerActiveBackgroundColor: '#e4baa1',
+        drawerInactiveTintColor: 'white',
       }}
     >
       <Drawer.Screen
         name='Categories'
-        component={CategoriesScreen} options={{
-        title: 'All Categories'
-      }}/>
+        component={CategoriesScreen}
+        options={{
+          title: 'All Categories',
+          drawerIcon: ({ size, color }) => (
+            <Ionicons
+              name='list'
+              size={size}
+              color={color}
+            />
+          )
+        }}
+      />
       <Drawer.Screen
         name='Favourites'
         component={FavouritesScreen}
+        options={{
+          title: 'Favourites',
+          drawerIcon: ({ size, color }) => (
+            <Ionicons
+              name='star'
+              size={size}
+              color={color}
+            />
+          )
+        }}
       />
     </Drawer.Navigator>
   )
